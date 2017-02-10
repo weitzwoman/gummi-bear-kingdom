a81292a5f8c029bc6f0e9ae49e306ecf3ed17faf
Tracie Weitzman <tlweitzman@gmail.com>
2017-02-10 08:53:47 -0800

    add validations to model and model spec

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   validates :name, :presence => true
+  validates :cost, :presence => true
+  validates :country_origin, :presence => true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Product do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+  it { should validate_presence_of :country_origin }
 end
