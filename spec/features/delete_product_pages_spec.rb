84d5fbde832d8ad8239eeacce72b59088c08934e
Tracie Weitzman <tlweitzman@gmail.com>
2017-02-10 09:09:12 -0800

    add passing spec for delete Product

---
+++ spec/features/delete_product_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "the delete product process" do
+  it "deletes a product" do
+    product = Product.create(:name => 'Super Sweets', :cost => 2, :country_origin => "Canada")
+    visit product_path(product)
+    click_on 'Delete Product'
+    expect(page).to_not have_content 'Super Sweets'
+  end
+end
