d9319da723f16c9d5a7ae090474c5c3c0b267861
Tracie Weitzman <tlweitzman@gmail.com>
2017-02-10 09:03:44 -0800

    add passing spec for add products

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    visit products_path
+    click_link 'Add New Product'
+    fill_in 'Name', :with => 'Not Chocolate'
+    fill_in 'Cost', :with => 2
+    fill_in 'Country origin', :with => 'USA'
+    click_on 'Create Product'
+    expect(page).to have_content 'All Inventory'
+  end
+
+  it "gives error when form field is left blank" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content 'errors'
+  end
+end
